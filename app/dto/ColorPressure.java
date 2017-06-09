17d4d4396253c0605242cfc5b119986ec3530210
TTCAD\a-yamamoto <a-yamamoto@ttc-net.co.jp>
2017-06-09 16:12:38 +0900

    pressureColor Color悩み中

--- app/dto/ColorPressure.java
+++ app/dto/ColorPressure.java
@@ -10,17 +10,41 @@ package dto;
  *
  * @author a-yamamoto
  */
+
+/**
+ * 気圧の値の変化で色を変化させる処理
+ */
 public class ColorPressure extends ColorChange {
     private static final String UNIT = "hPa";
     
     public ColorPressure(int pressure) {
-        if(pressure>=1000){
-            super.color = "#ff0000";
+        setColorPressure(pressure);
+        super.elem = pressure + UNIT;
+    }
+    
+    public void setColorPressure(int pressure) {
+        if(pressure >= 1016){
+            super.color = "#ffbf7f";
+        }
+        //1013hPaは平均の気圧の高さ
+        else if(pressure >= 1013){
+            super.color = "#ffc184";
+        }
+        else if(pressure >= 1010){
+            super.color = "#ffc489";
+        }
+        else if(pressure >= 1007){
+            super.color = "#ffc68e";
+        }
+        else if(pressure >= 1004){
+            super.color = "#ffc993";
+        }
+        else if(pressure >= 1001) {
+            super.color = "#ffcc99";
         }
         else{
-            super.color = "#00ff00";
+            super.color = "#ffedcc";
         }
-        super.elem = pressure + UNIT;
     }
 
 }
