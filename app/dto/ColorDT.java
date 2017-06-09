76b1558ef88e8ee5189bb2732f8deeb00065a86e
TTCAD\r-takahashi <r-takahashi@ttc-net.co.jp>
2017-06-09 14:39:27 +0900

    20170609　途中経過
    14：30時点

--- app/dto/ColorDT.java
+++ app/dto/ColorDT.java
@@ -10,5 +10,8 @@ package dto;
  * @author r-takahashi
  */
 public class ColorDT extends ColorChange{
-    
+    public ColorDT(int temp, int humidity){            
+        // 計算する 不快指数
+        DI = 0.81 * temp + 0.01 * humidity  * (0.99 * temp - 14.3) + 46.3
+    }
 }
