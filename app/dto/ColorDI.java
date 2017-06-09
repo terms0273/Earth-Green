2e574acba6e7dfd79b98fc49227c13e69da70f17
TTCAD\r-takahashi <r-takahashi@ttc-net.co.jp>
2017-06-09 16:23:28 +0900

    20170609

--- app/dto/ColorDI.java
+++ app/dto/ColorDI.java
@@ -5,7 +5,8 @@ package dto;
  * @author r-takahashi
  */
 public class ColorDI extends ColorChange{
-    public ColorDI(double temp, int humidity){            
+    public ColorDI(double temp, int humidity){
+        super.color = "#ff0000";
         // 計算する 不快指数
         super.elem = (0.81 * temp + 0.01 * humidity  * (0.99 * temp - 14.3) + 46.3) + "";
         
