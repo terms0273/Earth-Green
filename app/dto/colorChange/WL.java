b274527bfd385a0498ce763c50166044701dc121
TTCAD\r-takahashi <r-takahashi@ttc-net.co.jp>
2017-06-13 17:10:27 +0900

    20170613　表示位置の修正

--- app/dto/colorChange/WL.java
+++ app/dto/colorChange/WL.java
@@ -24,10 +24,13 @@ import org.apache.commons.lang3.time.DateFormatUtils;
 /**
  * 過去の天気情報を1時間ごとに一覧表示する DB内の情報が重複している場合、欠損している場合の対応をしていない（24時間分の表記がない日もある）
  * 東京の情報のみ表示
+ *
  * @author r-takahashi
  */
 public class WL {
+
     public List<String> str1;
+
     public WL(List<Weather> list) {
         // 各変数名は、DB内の表示名に従う
         str1 = new ArrayList<String>();
@@ -51,16 +54,17 @@ public class WL {
             pressure.add(new ColorPressure(l.pressure));
             humidity.add(new ColorHumidity(l.humidity));
             DI.add(new ColorDI(l.temp, l.humidity));
-            
+
         }
         map.put("ICON", icon);
         map.put("WEATHER", main);
         map.put("TEMPERETURE", temp);
-        map.put("AIR FROW", windSpeed);
-        map.put("CLOUD COVER", cloudAll);
-        map.put("PRESSURE", pressure);
         map.put("HUMIDITY", humidity);
         map.put("DI", DI);
+        map.put("AIR FROW", windSpeed);
+        map.put("PRESSURE", pressure);
+        map.put("CLOUD COVER", cloudAll);
+
     }
     public Map<String, List<ColorChange>> map;
 }
