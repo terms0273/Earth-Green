ac51d427d92e1f1adb25216b80e42f130952bd61
TTCAD\a-yamamoto <a-yamamoto@ttc-net.co.jp>
2017-06-14 16:13:11 +0900

    2017/06/14
    全項目に色付け完了

--- app/dto/colorChange/ColorHumidity.java
+++ app/dto/colorChange/ColorHumidity.java
@@ -8,7 +8,31 @@ package dto.colorChange;
 public class ColorHumidity extends ColorChange {
 
     public ColorHumidity(int humidity) {
-
+        setColorHumidity(humidity);
         super.elem = humidity + "%";
+    }
+    
+    public void setColorHumidity(int humidity) {
+        if (humidity >= 90) {
+            super.color = "#ffbeb3";
+        } else if (humidity >= 80) {
+            super.color = "#ffe4b3";
+        } else if (humidity >= 70) {
+            super.color = "#f4ffb3";
+        } else if (humidity >= 60) {
+            super.color = "#cdffb3";
+        } else if (humidity >= 50) {
+            super.color = "#b3ffbe";
+        } else if (humidity >= 40) {
+            super.color = "#b3ffe4";
+        } else if (humidity >= 30) {
+            super.color = "#b3f4ff";
+        } else if (humidity >= 20) {
+            super.color = "#b3cdff";
+        } else if (humidity >= 10) {
+            super.color = "#beb3ff";
+        } else {
+            super.color = "#e4b3ff";
+        }
     }
 }
